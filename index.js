cc6c1e2d03a772540d4e22ddc0d86e565bffcad6
Ariel Barcellos <barcellos.ariel@gmail.com>
2019-03-25 16:17:32 -0500

    added dispatch calls

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const { createStore } = require('./redux')
 const reducer = require('./reducer')
 
-const store = createStore(reducer)
+const store = createStore(reducer, 0)
 const state = store.getState()
 
 store.subscribe(() => console.log('The state has changed!', store.getState()))
+
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+
+store.dispatch({ type: 'DECREMENT' })
+store.dispatch({ type: 'DECREMENT' })
+store.dispatch({ type: 'DECREMENT' })
+store.dispatch({ type: 'DECREMENT' })
