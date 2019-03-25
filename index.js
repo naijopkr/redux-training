6a41473d7f91f474536596e504515835e131e12e
Ariel Barcellos <barcellos.ariel@gmail.com>
2019-03-25 15:45:54 -0500

    implemented subscribe to state changes

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ import { createStore } from './redux'
 
 const store = createStore()
 const state = store.getState()
+
+store.subscribe(() => console.log('The state has changed!', store.getState()))
--- redux.js
+++ redux.js
@@ -1,10 +1,15 @@
 export const createStore = (initialReducer, intiialState = {}) => {
   let reducer = initialReducer
+  let subscribers = []
   let state = reducer(intiialState, { type: '__INIT__' })
   return {
     getState: () => state,
-    dispatch: (action) => {
+    dispatch: action => {
       state = reducer(state, action)
+      subscribers.forEach(subscriber => subscriber())
+    },
+    subscribe = listener => {
+      subscribers.push(listener)
     }
   }
 }
