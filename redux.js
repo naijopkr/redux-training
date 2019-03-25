42e9b3f019f958de637e03146f70f561745ed5af
Ariel Barcellos <barcellos.ariel@gmail.com>
2019-03-25 15:41:19 -0500

    implemented dispatch actions

--- redux.js
+++ redux.js
@@ -2,6 +2,9 @@ export const createStore = (initialReducer, intiialState = {}) => {
   let reducer = initialReducer
   let state = reducer(intiialState, { type: '__INIT__' })
   return {
-    getState: () => state
+    getState: () => state,
+    dispatch: (action) => {
+      state = reducer(state, action)
+    }
   }
 }
